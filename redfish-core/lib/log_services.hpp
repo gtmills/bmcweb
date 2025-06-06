c3c8e2129c6b82be3039c866c8fe6b7950f14c3b
Chinmay Shripad Hegde <hosmanechinmay@gmail.com>
2025-06-06 16:23:47 +0530

    Task: Fix missing properties in task monitor
    Task monitor URI when the task is running contains partial information.
    Properties like progress percent, message is missing. Same issue is
    present when task is created and response is returned.
    
    This patch fixes missing properties in task creation response and task
    monitor URI.
    
    Tested:
      - Task creation with fwupdate response shows all properties
      - Task monitor URI provides complete information when running
      - Verified task creation and monitor response for log service
    
    Change-Id: I77c6d9b9302d13b8480cf5a3419b8c6754e5049d
    Signed-off-by: Chinmay Shripad Hegde <hosmanechinmay@gmail.com>
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- redfish-core/lib/log_services.hpp
+++ redfish-core/lib/log_services.hpp
@@ -1024,8 +1024,8 @@ inline void createDumpTaskCallback(
             // The task timer is set to max time limit within which the
             // requested dump will be collected.
             task->startTimer(std::chrono::minutes(20));
-            task->populateResp(asyncResp->res);
             task->payload.emplace(payload);
+            task->populateResp(asyncResp->res);
         },
         "xyz.openbmc_project.Dump.Manager", createdObjPath,
         "org.freedesktop.DBus.Introspectable", "Introspect");
@@ -3545,8 +3545,8 @@ inline void requestRoutesCrashdumpCollect(App& app)
                                 taskMatchStr);
 
                         task->startTimer(std::chrono::minutes(5));
-                        task->populateResp(asyncResp->res);
                         task->payload.emplace(std::move(payload));
+                        task->populateResp(asyncResp->res);
                     };
 
                 dbus::utility::async_method_call(
